38777ffd74a6baf6cc8722201ca860699d865f58
Taysseer Kadri <taysseer.kadri@gmail.com>
2022-12-16 21:53:53 -0800

    first commit attempt

---
+++ imports/ui/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { ContactForm } from './ContactForm';
+
+
+//Contact: name, email, imageURL
+export const App = () => (
+  <div>
+    <h1>Meteor Wallet!</h1>
+    <ContactForm />
+  </div>
+);
